91f019d083204d5ea852caa89913ce0d07014342
Imane Elouazzani <denjala.smiley@gmail.com>
2022-01-19 21:44:21 +0100

    props bien pratiqués

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { plantList } from '../datas/plantList'
 import '../styles/ShoppingList.css'
+//import {v4 as uuid4} from 'uuid'
+import PlantItem from './PlantItem'
 
 export default function ShoppingList() {
     const categories = plantList.reduce(
 		(acc, plant) =>
-			acc.includes(plant.category) ? acc : acc.concat(plant.category)
-            , []
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),[]
+            
 	)
     return (
         <div>
@@ -16,11 +18,14 @@ export default function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
-						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-					</li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
         </div>
